ada8d321ed3915f327317a172b552661032ff0f9
cemilcelik <cemilcelik84@gmail.com>
2017-08-24 17:12:13 +0300

    Debugbar added.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $providers = [
+        'Barryvdh\Debugbar\ServiceProvider'
+    ];
+
+    protected $aliases = [
+        'Debugbar' => 'Barryvdh\Debugbar\Facade'
+    ];
+
     /**
      * Bootstrap any application services.
      *
@@ -24,6 +32,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal() && ! empty($this->providers)) {
+            foreach ($this->providers as $provider) {
+                $this->app->register($provider);
+            }
+            if ( ! empty($this->aliases)) {
+                foreach ($this->aliases as $alias => $facade) {
+                    $this->app->alias($alias, $facade);
+                }
+            }
+        }
     }
 }
